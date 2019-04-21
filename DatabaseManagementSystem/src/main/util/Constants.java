8f2038b366ebe53463d23d43df32ec8465f065cd
Dan Cazacu <dan.gabriel.cazacu@gmail.com>
2019-04-21 20:52:42 +0300

    implemented insert into table

--- DatabaseManagementSystem/src/main/util/Constants.java
+++ DatabaseManagementSystem/src/main/util/Constants.java
@@ -14,4 +14,5 @@ public class Constants {
     public static final String NAME = "name";
     public static final String DATA = "data";
     public static final String SET = "set";
+    public static final String VALUE = "value";
 }
