6f4ee3a784935fd703d3bc245931ede35bc9b6a9
Dan Cazacu <dcazacu@salesforce.com>
2019-05-12 17:08:15 +0300

    Added persistance test and Delete from command line test

--- DatabaseManagementSystem/src/main/Main.java
+++ DatabaseManagementSystem/src/main/Main.java
@@ -1,18 +1,26 @@
 package main;
 
+import main.exception.InvalidCommand;
 import main.graphicalInterface.MainWindow;
 import main.persistance.DatabasePersistance;
 import main.service.CommandLineParser;
 
+
+/**
+ * Tests dependencies needed
+ * org.mockito:mockito-core:2.27.0
+ * org.junit.jupiter:junit-jupiter-params:5.4.2
+ */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidCommand {
 
         MainWindow mainFrame = new MainWindow();
         mainFrame.open();
         CommandLineParser commandLineParser = new CommandLineParser();
-        commandLineParser.startCommandLine();
+        while(true)
+            commandLineParser.startCommandLine();
 
-        new DatabasePersistance().persist();
+//        new DatabasePersistance().persist();
     }
 }
---
+++ DatabaseManagementSystem/tests/persistence/PersistenceTest.java
@@ -0,0 +1,46 @@
+package persistence;
+
+import main.exception.AlreadyExists;
+import main.exception.DoesNotExist;
+import main.exception.InvalidValue;
+import main.model.Database;
+import main.model.DatabaseManagementSystem;
+import main.persistance.DatabasePersistance;
+import main.util.DataBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersistenceTest {
+
+    @Test
+    public void loadPersistTest(){
+
+        //auto loads data from file
+        int primaryHashCode = DatabaseManagementSystem.getInstance().hashCode();
+        List<Database> databases = new ArrayList<>(DatabaseManagementSystem.getInstance().getDatabases());
+        DatabasePersistance persistance = new DatabasePersistance();
+        persistance.persist();
+
+        DatabaseManagementSystem.getInstance().getDatabases().clear();
+        try {
+            persistance.load();
+        } catch (IOException | InvalidValue | AlreadyExists e) {
+            fail(e);
+        }
+
+        for (Database database : databases) {
+            try {
+                DatabaseManagementSystem.getInstance().getDatabase(database.getName());
+            } catch (DoesNotExist doesNotExist) {
+                fail(doesNotExist);
+            }
+        }
+
+    }
+
+}
